0eab0e9b2c3977246de2822b5ac302160aae196f
Jmetev1 <jmetevier@gmail.com>
2019-11-04 11:24:20 -0800

    install on server but don't build

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express(),
   path = require('path'),
-  publicDir = path.join(__dirname, 'public');
+  buildDir = path.join(__dirname, 'build');
 
-app.use(express.static(publicDir))
+app.use(express.static(buildDir))
 
 app.listen(port);
 module.exports = app;
--- tests/test.js
+++ tests/test.js
@@ -9,6 +9,7 @@ const request = supertest(app);
 describe('Tests app', function () {
   it('verifies get', function (done) {
     request.get('/').expect(200).end(function (err, result) {
+      console.log(12, err)
       test.string(result.text).contains('root');
       test.value(result).hasHeader('content-type', 'text/html; charset=UTF-8');
       done(err);
