786632a13e80adbd5835fbdc86867dfd9180fc81
Jmetev1 <jmetevier@gmail.com>
2019-11-04 09:33:32 -0800

    now serving no react

---
+++ oldserver.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path')
+
+let reload
+const http = require('http');
+const bodyParser = require('body-parser');
+const session = require('express-session')
+// const dotenv = require('dotenv');
+// dotenv.load();
+const development = false
+// const development = process.env.NODE_ENV !== 'PRODUCTION'
+console.log(13, development, process.env.NODE_ENV, process.env.NODE_ENV !== 'PRODUCTION')
+// if (development) {
+//   console.log('not prodc')
+//   reload = require('reload');
+// }
+const util = require('./util');
+const port = process.env.PORT || 3000
+console.log(18, port)
+app.set('port', port);
+app.use(session({
+  name: 'server-session-cookie-id',
+  secret: 'my express secret',
+  saveUninitialized: true,
+  resave: false,
+}));
+
+app.use(bodyParser.json());
+console.log('env variables are', process.env.NODE_ENV, process.env.PORT)
+// publicDir = path.join(__dirname, 'public');
+
+// app.use(express.static(publicDir))
+// if (process.env.NODE_ENV !== "DEVELOPMENT") {
+const buildDir = path.join(__dirname, 'build');
+app.use(express.static(buildDir));
+// }
+const map = {
+  "North Louisiana": "nl",
+  "North Mississippi": "nm",
+  "South Louisiana": "sl",
+  "South Mississippi": 'sm'
+}
+
+app.options('/login', cors());
+app.get('/login', cors(), (req, res) => {
+  if (req.session.rep !== 'nm') {
+    req.session.rep = 'nm'
+  }
+  console.log(req.session.rep)
+  const info = {
+    rep: req.session.rep,
+    env: process.env.NODE_ENV
+  }
+  res.send(JSON.stringify(info))
+})
+app.post('/login', cors(), (req, res) => {
+  const rep = map[req.body.rep] || false;
+  req.session.rep = rep
+  console.log(47, req.session)
+  res.send(JSON.stringify(rep))
+})
+app.options('/visit', cors());
+app.post('/visit', cors(), async (req, res) => {
+  req.body.rep = req.session.rep
+  console.log(21, req.body);
+  // console.log()
+  const dbres = await util.addVisit(req.body);
+  // console.log(21, dbres);
+  res.end(JSON.stringify('s'));
+});
+app.get('/visits', cors(), async (req, res) => {
+  // const dbres = await util.getVisits();
+  const dbres = await util.checkSpending(req.session.rep);
+  res.json([dbres]);
+});
+app.options('/clinic', cors());
+
+
+
+
+
+
+
+
+
+
+app.options('/provider', cors());
+app.get('/getproviders', cors(), async (req, res) => {
+  const dbres = await util.providersByRep(req.session.rep);
+  res.end(JSON.stringify(dbres))
+})
+/*
+pass array of clinics to get all prviders for each.
+*/
+app.post('/provider', cors(), async (req, res) => {
+  console.log('add provider', req.body, req.session);
+  const dbres = await util.addProvider({
+    ...req.body,
+    rep: req.session.rep
+  })
+  res.end(JSON.stringify('provider success'));
+});
+
+app.post('/clinic', cors(), async (req, res) => {
+  console.log('add clinic', req.body, req.session);
+  const dbres = await util.addClinic({
+    ...req.body,
+    rep: req.session.rep
+  })
+  res.end(JSON.stringify('clinic success'));
+});
+
+app.get('/clinic', cors(), async (req, res) => {
+  const allClinics = await util.getClinic(req.session.rep)
+  res.end(JSON.stringify(allClinics));
+});
+// if (port === "3000") {
+//   console.log(110, development)
+//   app.get('*', cors(), (req, res) => {
+//     res.end('no route')
+//   })
+// }
+
+const server = http.createServer(app);
+if (development) {
+  reload(app).then(() => {
+    server.listen(app.get('port'), () => {
+      console.log(`Web server listening on port ${app.get('port')}`);
+    });
+  }).catch((err) => {
+    console.error('Reload could not start, could not start server/sample app', err);
+  });
+} else {
+  server.listen(app.get('port'), () => {
+    console.log(`Web server listening on port ${app.get('port')}`);
+  });
+}
+
+module.exports = app;
--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const supertest = require('supertest');
 const test = require('unit.js');
@@ -7,12 +7,11 @@ const app = require('../server.js');
 const request = supertest(app);
 
 describe('Tests app', function () {
-  // it('verifies get', function (done) {
-  //   request.get('/').expect(200).end(function (err, result) {
-  //     console.log(result)
-  //     test.string(result.text).contains('Congratulations');
-  //     test.value(result).hasHeader('content-type', 'text/html; charset=UTF-8');
-  //     done(err);
-  //   });
-  // });
+  it('verifies get', function (done) {
+    request.get('/').expect(200).end(function (err, result) {
+      test.string(result.text).contains('root');
+      test.value(result).hasHeader('content-type', 'text/html; charset=UTF-8');
+      done(err);
+    });
+  });
 });
